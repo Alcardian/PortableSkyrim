db19c56dbed59908004325309d532ec781f10874
Alcardian <alcardian@gmail.com>
2025-07-04 18:10:47 +0200

    added run scripts

---
+++ scripts/Run_Skyrim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Set Wine prefix and run Mod Organizer 2 for Skyrim
+WINEPREFIX="$HOME/Games/Skyrim_Portable/WinePrefix" wine "$HOME/Games/Skyrim_Portable/MO2/ModOrganizer.exe"
---
+++ scripts/Run_SkyrimLauncher.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Set Wine prefix and run Skyrim Launcher
+WINEPREFIX="$HOME/Games/Skyrim_Portable/WinePrefix" wine "$HOME/Games/Skyrim_Portable/WinePrefix/drive_c/GOG Games/Skyrim Anniversary Edition/SkyrimSELauncher.exe"
